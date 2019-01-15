52ad2ae527fb3c1caad6bd61c059ad39b3a3d6a6
Sudharshana S L <15.hari.hari@gmail.com>
2019-01-15 14:46:46 +0530

    first component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core'
 
 @Component({
-  selector: 'app-root',
-  styleUrls: ['app.component.scss'],
+  // creates html element for the component
+  selector: "app-root",
+  styleUrls: ["app.component.scss"],
+  // back ticks are es6 feature to serve as multi line strings
+  // assigning a template to particular component
   template: `
-    <div class="app">
-      Hello!
-    </div>
+  <div class="app">
+    <!--this is called interpolation-->
+    {{title}}
+  </div>
   `
 })
+/**
+ * Component decorator gets bound to this app component
+ * All of the properties exposed here will be available in template
+ */
 export class AppComponent {
-
+  title: string;
+  constructor() {
+    this.title  = "Sudharshana";
+  }
 }
--- main.ts
+++ main.ts
@@ -3,3 +3,41 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
+
+function ShoppingList() {
+    this.groceries = [];
+}
+
+ShoppingList.prototype.addItem = function (item) {
+
+    // this is this way
+    // this.groceries.push(item);
+
+    // angular way - it prefers this immutable way
+    // makes change detection easy
+    this.groceries = this.groceries.concat([item]);
+}
+
+ShoppingList.prototype.removeItem = function (item) {
+
+    // angular way - it prefers this immutable way
+    // makes change detection easy
+    this.groceries = this.groceries.filter(function (grocery) {
+        return grocery != item;
+    })
+}
+
+var myList = new ShoppingList();
+
+myList.addItem("Banana");
+myList.addItem("Apple")
+
+console.log(myList.groceries)
+
+myList.removeItem("Apple")
+console.log(myList.groceries)
+
+
+// class ABCD {
+// 	abc: string;
+// }
