0158a46087f80248a3ee58e1d99dbfa10bc3e261
Sudharshana S L <15.hari.hari@gmail.com>
2019-01-15 17:15:35 +0530

    bootstrapping on client side, had missed to commit app.component.html

--- main.ts
+++ main.ts
@@ -1,43 +1,10 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+// there are several ways we can bootstrap module in angular
+// we can tell angular to bootstrap on server or client or precompile it
 
-import { AppModule } from './app/app.module';
+// dynamic means client side, our code will be compiled in the browser
+// it will contain client side code that processes templates, bindings and allows us to do dependency injections
+import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+import {AppModule} from './app/app.module';
 
-function ShoppingList() {
-    this.groceries = [];
-}
-
-ShoppingList.prototype.addItem = function (item) {
-
-    // this is this way
-    // this.groceries.push(item);
-
-    // angular way - it prefers this immutable way
-    // makes change detection easy
-    this.groceries = this.groceries.concat([item]);
-}
-
-ShoppingList.prototype.removeItem = function (item) {
-
-    // angular way - it prefers this immutable way
-    // makes change detection easy
-    this.groceries = this.groceries.filter(function (grocery) {
-        return grocery != item;
-    })
-}
-
-var myList = new ShoppingList();
-
-myList.addItem("Banana");
-myList.addItem("Apple")
-
-console.log(myList.groceries)
-
-myList.removeItem("Apple")
-console.log(myList.groceries)
-
-
-// class ABCD {
-// 	abc: string;
-// }
+platformBrowserDynamic().bootstrapModule(AppModule)
