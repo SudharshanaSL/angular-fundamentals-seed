cc0a95381e2fc825d0a180fa256ae52da1fc731e
Sudharshana S L <15.hari.hari@gmail.com>
2019-02-09 11:52:38 +0530

    ngFor lesson

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,6 +5,12 @@
 
 import {Component} from '@angular/core'
 
+interface Passenger {
+  id: number,
+  name: string,
+  checkedIn: boolean
+}
+
 @Component({
   // creates html element for the component
   selector: "app-root",
@@ -28,8 +34,32 @@ import {Component} from '@angular/core'
         <div *ngIf="name.length >= 3">
           Searching for... {{name}}
         </div>
+        <hr>
+        <h1>ngFor</h1>
+        <h3>Airline Passengers</h3>
+        <ul>
+          <h2>structural ngFor</h2>
+          <li *ngFor="let passenger of passengers; let i = index">
+            <hr>
+            {{i}} :
+            {{passenger.id}},
+            {{passenger.name}},
+            {{passenger.checkedIn}}
+          </li>
+        </ul>
+        <hr>
+        <ul>
+          <h2>web components offer data encapsulation</h2>
+          <template ngFor let-passenger let-i="index" [ngForOf]="passengers">
+            <li>
+              <hr>
+              {{i+1}} :
+              {{passenger.name}},
+              {{passenger.checkedIn}}
+            </li>
+          </template>
+        </ul>
       </div>
-      <hr>
     `
 })
 /**
@@ -41,4 +71,25 @@ export class AppComponent {
   handleChange(value: string) {
     this.name = value;
   }
+  passengers: Passenger[] = [{
+    id: 1,
+    name: this.name+1,
+    checkedIn: false
+  }, {
+    id:2,
+    name: this.name+2,
+    checkedIn: true
+  }, {
+    id:3,
+    name: this.name+2,
+    checkedIn: false
+  }, {
+    id:4,
+    name: this.name+2,
+    checkedIn: false
+  }, {
+    id:5,
+    name: this.name+2,
+    checkedIn: true
+  }]
 }
