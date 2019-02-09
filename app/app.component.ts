1cdeee63e6e5877db5343575d420e15f32d6e35b
Sudharshana S L <15.hari.hari@gmail.com>
2019-02-09 13:11:25 +0530

    safe navigation operator - ?

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,12 +5,18 @@
 
 import {Component} from '@angular/core'
 
+interface Children {
+  name: string,
+  age: number
+}
+
 interface Passenger {
   id: number,
   name: string,
   checkedIn: boolean,
   mandatoryCheckedInDate: number | null,
-  optionalCheckedInDate?: number
+  optionalCheckedInDate?: number,
+  children: Children[] | null
 }
 
 @Component({
@@ -34,6 +40,9 @@ interface Passenger {
             Checked in date: 
             {{passenger.checkedIn? (passenger.optionalCheckedInDate | date: 'dd-MMM-yyyy-GG' | uppercase): 'Not checked in'}}
           </div>
+          <div class="children">
+            Children: {{passenger.children?.length || 0}}
+          </div>
         </li>
       </ul>
       <hr>
@@ -54,27 +63,44 @@ export class AppComponent {
     name: this.name+1,
     checkedIn: false,
     mandatoryCheckedInDate: 1490742111111,
-    optionalCheckedInDate: 1490742111111
+    optionalCheckedInDate: 1490742111111,
+    children: null
   }, {
     id:2,
     name: this.name+2,
     checkedIn: true,
-    mandatoryCheckedInDate: null
+    mandatoryCheckedInDate: null,
+    children: [{
+      name: "abcd",
+      age: 12
+    }, {
+      name: "efgh",
+      age: 3
+    }]
   }, {
     id:3,
     name: this.name+3,
     checkedIn: true,
     mandatoryCheckedInDate: 1490742000004,
-    optionalCheckedInDate: 1490742000004
+    optionalCheckedInDate: 1490742000004,
+    children: null
   }, {
     id:undefined,
     name: this.name+4,
     checkedIn: false,
-    mandatoryCheckedInDate: null
+    mandatoryCheckedInDate: null,
+    children: null
   }, {
     id:5,
     name: this.name+5,
     checkedIn: undefined,
-    mandatoryCheckedInDate: null
+    mandatoryCheckedInDate: null,
+    children: [{
+      name: "abcd",
+      age: 12
+    }, {
+      name: "efgh",
+      age: 3
+    }]
   }]
 }
