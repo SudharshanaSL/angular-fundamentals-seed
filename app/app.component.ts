40e783882a1b6eee2ebfb0eee7d7bb6a5c7768f7
Sudharshana S L <15.hari.hari@gmail.com>
2019-02-09 13:03:36 +0530

    pipe + chaining

--- app/app.component.ts
+++ app/app.component.ts
@@ -8,7 +8,9 @@ import {Component} from '@angular/core'
 interface Passenger {
   id: number,
   name: string,
-  checkedIn: boolean
+  checkedIn: boolean,
+  mandatoryCheckedInDate: number | null,
+  optionalCheckedInDate?: number
 }
 
 @Component({
@@ -17,60 +19,24 @@ interface Passenger {
   styleUrls: ["app.component.scss"],
   template: ` <!-- Also referred as View -->
       <div class="app">
-        <input
-          type="text"
-          [value]="name"
-          (input)="handleChange($event.target.value)">
-        <h1>ngFor</h1>
-        <h3>Airline Passengers</h3>
-        <ul>
-          <h2>structural ngFor</h2>
-          <li *ngFor="let passenger of passengers; let i = index">
-            <span
-              class="status"
-              [class.checked-in]="passenger.checkedIn"></span>
-            {{i}} : {{passenger.name}}
-          </li>
-        </ul>
-        <hr>
-        <ul>
-          <h2>structural ngFor</h2>
-          <li *ngFor="let passenger of passengers; let i = index">
-            <span
-              class="status"
-              [ngClass]="{ 
-                'checked-in': passenger.checkedIn,
-                'cancelled' : passenger.checkedIn == undefined,
-                'did-not-book': passenger.id == undefined }">
-              </span>
-            {{i}} : {{passenger.name}}
-          </li>
-        </ul>
-        <hr>
-        <ul>
-          <h2>style</h2>
-          <li *ngFor="let passenger of passengers; let i = index">
-            <span
-              class="status"
-              [ngStyle]="{ 
-                backgroundColor: (passenger.checkedIn ? 'green':'#c0392b')}">
-              </span>
-              element.style.background = 'red'
-            {{i}} : {{passenger.name}}
-          </li>
-        </ul>
-        <hr>
-        <ul>
-          <h2>ngStyle</h2>
-          <li *ngFor="let passenger of passengers; let i = index">
-            <span
-              class="status"
-              [style.backgroundColor]="(passenger.checkedIn ? 'green':'#c0392b')">
-              </span>
-              element.style.background = 'red'
-            {{i}} : {{passenger.name}}
-          </li>
-        </ul>
+      <h3>Airline Passengers</h3>
+      <ul>
+        <h2>structural ngFor</h2>
+        <li *ngFor="let passenger of passengers; let i = index">
+          <span
+            class="status"
+            [class.checked-in]="passenger.checkedIn"></span>
+          {{i}} : {{passenger.name}}
+          <!--we tell angular that we need the json pipe with passenger being input-->
+          <p>{{passenger | json}}</p>
+          <div class="date">
+            <!--pipe will not output anything if value is null-->
+            Checked in date: 
+            {{passenger.checkedIn? (passenger.optionalCheckedInDate | date: 'dd-MMM-yyyy-GG' | uppercase): 'Not checked in'}}
+          </div>
+        </li>
+      </ul>
+      <hr>
       </div>
     `
 })
@@ -79,29 +45,36 @@ interface Passenger {
  * All of the properties exposed here will be available in template
  */
 export class AppComponent {
-  name: string = ""
+  name: string = "pass"
   handleChange(value: string) {
     this.name = value;
   }
   passengers: Passenger[] = [{
     id: 1,
     name: this.name+1,
-    checkedIn: false
+    checkedIn: false,
+    mandatoryCheckedInDate: 1490742111111,
+    optionalCheckedInDate: 1490742111111
   }, {
     id:2,
     name: this.name+2,
-    checkedIn: true
+    checkedIn: true,
+    mandatoryCheckedInDate: null
   }, {
     id:3,
     name: this.name+3,
-    checkedIn: false
+    checkedIn: true,
+    mandatoryCheckedInDate: 1490742000004,
+    optionalCheckedInDate: 1490742000004
   }, {
     id:undefined,
     name: this.name+4,
-    checkedIn: false
+    checkedIn: false,
+    mandatoryCheckedInDate: null
   }, {
     id:5,
     name: this.name+5,
-    checkedIn: undefined
+    checkedIn: undefined,
+    mandatoryCheckedInDate: null
   }]
 }
