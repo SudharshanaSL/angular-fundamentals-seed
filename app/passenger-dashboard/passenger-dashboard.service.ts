99d04ff73a7c7dde90543505a2f308938ed3b679
Sudharshana S L <15.hari.hari@gmail.com>
2019-02-10 18:19:55 +0530

    custom headers - custom requests

--- app/passenger-dashboard/passenger-dashboard.service.ts
+++ app/passenger-dashboard/passenger-dashboard.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-import { Http, Response } from '@angular/http'
+import { Http, Response, Headers, RequestOptions } from '@angular/http'
 
 import { Observable } from 'rxjs/Observable'
 import 'rxjs/add/operator/map'
@@ -23,8 +23,14 @@ export class PassengerDashboardService {
     }
 
     updatePassenger(passenger: Passenger): Observable<Passenger> {
+        let headers = new Headers({
+            'Content-Type': 'application/json'
+        })
+        let options = new RequestOptions({
+            headers: headers
+        })
         return this.http
-            .put(`${PASSENGER_API}/${passenger.id}`, passenger)
+            .put(`${PASSENGER_API}/${passenger.id}`, passenger, options)
             .map((response: Response) =>
                 response.json()
             )
